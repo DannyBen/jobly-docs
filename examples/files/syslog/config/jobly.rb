4ce0cdbcc946728850401daf743500d55c296aa2
Danny Ben Shitrit <db@dannyben.com>
2019-03-16 09:26:03 +0000

    update syslog example

--- examples/files/syslog/config/jobly.rb
+++ examples/files/syslog/config/jobly.rb
@@ -1,16 +1,5 @@
-require 'remote_syslog_logger'
-
 Jobly.configure do |config|
-
-  # Configure syslog parameters
-  syslog_host = ENV['SYSLOG_HOST'] || "127.0.0.1"
-  syslog_port = ENV['SYSLOG_PORT'] || '514'
-  sysname, program = 'my-host', 'my-app'
-
-  # Set the Jobly logger to syslog
-  config.logger = RemoteSyslogLogger.new syslog_host, syslog_port, 
-    local_hostname: sysname, program: program
-
+  # Log to local syslog and use the system name `jobly` and tag `demo`
+  config.log = "syslog://jobly:demo@127.0.0.1:514"
   config.logger.level = Logger::INFO
-
 end
