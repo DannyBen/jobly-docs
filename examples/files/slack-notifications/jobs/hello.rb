2ee526f7d8c3463955c43107b04bbdc90d455d52
Danny Ben Shitrit <db@dannyben.com>
2019-03-22 08:14:23 +0000

    zip examples

--- examples/files/slack-notifications/jobs/hello.rb
+++ examples/files/slack-notifications/jobs/hello.rb
@@ -1,4 +1,8 @@
 class Greet < Job
+  # Optional settings (defaults to #debug and Jobly)
+  slack_channel '#debug'
+  slack_user 'Greeter'
+
   def execute(name: 'bob')
     # Quick message
     slack.ping "Hello #{name}"
